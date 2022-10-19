b468d252dd639b11cd868d53cd862409ce6a9406
Tim Menzies <timm@ieee.org>
2022-10-18 20:58:32 -0400

    saving

--- near.lua
+++ near.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env lua
 -- Test engine and examples.
 -- (c)2022 Tim Menzies <timm@ieee.org> BSD2
-local _  = require"glua"
+local _  = require"4all"
 local cli,oo,rand,rint,rnd = _.cli, _.oo, _.rand, _.rint, _.rnd
 local run,sort,srand    = _.run, _.sort, _.srand
 
